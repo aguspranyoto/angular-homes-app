f9f610535d9d90e6f475cf4959fe97781c8ed08b
aguspranyoto <agusprnyt@gmail.com>
2023-11-29 22:48:55 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HomeComponent, AppRoutingModule, RouterModule],
+  providers: [provideRouter(routeConfig)],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
